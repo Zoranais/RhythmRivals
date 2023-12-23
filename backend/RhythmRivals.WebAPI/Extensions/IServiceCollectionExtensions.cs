6b89f624333b3ee466bbcfbdd4378e6312dbc48b
Zoranais <unikronusus@gmail.com>
2023-12-23 14:13:11 +0200

    Add typed clients

--- backend/RhythmRivals.WebAPI/Extensions/IServiceCollectionExtensions.cs
+++ backend/RhythmRivals.WebAPI/Extensions/IServiceCollectionExtensions.cs
@@ -4,9 +4,12 @@ using RhythmRivals.BLL.Interfaces;
 using RhythmRivals.BLL.Jobs;
 using RhythmRivals.BLL.MappingProfiles;
 using RhythmRivals.BLL.Services;
+using RhythmRivals.BLL.Services.HttpClients;
 using RhythmRivals.WebAPI.ExceptionHandlers;
 using RhythmRivals.WebAPI.Validators;
 using Serilog;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace RhythmRivals.WebAPI.Extensions;
 
@@ -15,7 +18,6 @@ public static class IServiceCollectionExtensions
     public static void AddCustomServices(this IServiceCollection services)
     {
         services.AddScoped<IGameService, GameService>();
-        services.AddScoped<IHttpService, HttpService>();
         services.AddScoped<IPlayerService, PlayerService>();
         services.AddScoped<IMusicService, SpotifyMusicService>();
 
@@ -50,6 +52,25 @@ public static class IServiceCollectionExtensions
             //o.WriteTo.Seq(configuration["Serilog:SeqUrl"] ?? throw new ConfigurationNotProvidedException("SeqUrl")); // without SEQ for now
             o.Enrich.FromLogContext();
             o.Enrich.WithMachineName();
+        });
+    }
+
+    public static void AddSpotifyHttpClient(this IServiceCollection services)
+    {
+        services.AddHttpClient<SpotifyApiService>((options) =>
+        {
+            options.BaseAddress = new Uri("https://api.spotify.com/v1/");
+        });
+    }
+    public static void AddSpotifyAuthorizationHttpClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient<SpotifyAuthorizationApiService>((options) =>
+        {
+            var clientId = configuration["Client_Id"] ?? string.Empty;
+            var clientSecret = configuration["Client_Secret"] ?? string.Empty;
+            options.BaseAddress = new Uri("https://accounts.spotify.com/api/");
+            options.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                $"{Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"))}");
         });
     }
 
